9de1d2604c504761350a6edf7d0a4f8ed363b092
Gabriel Wilkinson <gabriel.wilkinson@hotmail.com>
2023-02-27 15:42:31 +0000

    playing around with classes and importing files

---
+++ animals/animal_def.py
@@ -0,0 +1,4 @@
+class Animal:
+
+    def print_animal(self):
+        return 'This is animal class'
---
+++ animals/file.py
@@ -0,0 +1,7 @@
+from animal_def import Animal
+
+# create object of class
+animal = Animal()
+
+# call class method
+print(animal.print_animal())
