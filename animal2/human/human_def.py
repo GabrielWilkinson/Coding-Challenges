ee42f2dd0bcfa71773597a8300c53534336ef947
Gabriel Wilkinson <gabriel.wilkinson@hotmail.com>
2023-02-27 15:59:02 +0000

    playing around with classes and importing from different file structures

---
+++ animal2/human/human_def.py
@@ -0,0 +1,7 @@
+class Animal:
+    def print_animal(self):
+        return 'This is animal class'
+
+class Reptile:
+    def print_reptile(Self):
+        return "This is reptile class"
---
+++ animal2/run.py
@@ -0,0 +1,11 @@
+from human.human_def import *
+
+# create object of Animal class
+animal = Animal()
+
+# create object of Reptile class
+reptile = Reptile()
+
+# call class method
+print(animal.print_animal())
+print(reptile.print_reptile())
