695afe9e24a263bb5da3089046bd78e9fb7ffba8
Gabriel Wilkinson <gabriel.wilkinson@hotmail.com>
2023-02-27 18:37:15 +0000

    revising 'self'

---
+++ democlass/democlass.py
@@ -0,0 +1,9 @@
+class DemoClass:
+
+    def demo_method(self):
+        print("I am a demonstration method")
+        print(id(self))
+
+my_object = DemoClass()
+print(id(my_object))
+my_object.demo_method()
